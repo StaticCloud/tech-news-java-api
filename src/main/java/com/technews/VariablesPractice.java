55793dcee9f9ec7fe3b85bc91995e42f96bd080c
DMarrs <diegomarrs@gmail.com>
2022-12-28 14:11:11 -0800

    added post and user model

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int number_1 = 2;
+        double number_2 = 2.5;
+        double sum = number_1 + number_2;
+
+        String greetings = "Hello, World!";
+
+        String intString = number_1 + greetings;
+
+        System.out.println(sum);
+        System.out.println(intString);
+    }
+}
